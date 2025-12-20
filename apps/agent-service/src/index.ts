5bb109de8046bf29d478d7486e1daedd9b4abd36
Mayank Gupta <mayank.gupta@cube.ms>
2025-12-20 12:43:36 +0530

    Docker working perfeectly

--- apps/agent-service/src/index.ts
+++ apps/agent-service/src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import connectDB from "./lib/db";
-import { redisClient } from "./lib/redis";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 const startServer = async () => {
   await connectDB();
-  await redisClient.connect();
+
   app.listen(PORT, () => {
     console.log(
       "Weather agent service connected to DB and is running on PORT:",
