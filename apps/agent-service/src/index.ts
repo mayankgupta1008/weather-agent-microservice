f8192515bc3783a49eebc647c138160c8e892bc7
Mayank Gupta <mayank.gupta@cube.ms>
2025-12-25 12:36:23 +0530

    renamed PORT to AGENT_PORT

--- apps/agent-service/src/index.ts
+++ apps/agent-service/src/index.ts
@@ -2,8 +2,6 @@ import express from "express";
 import connectDB from "@weather-agent/shared/src/common/db.config.js";
 import "@weather-agent/shared/src/common/redis.config.js";
 import cors from "cors";
-import { requireAuth } from "@weather-agent/shared/src/common/auth.middleware.js";
-import { toNodeHandler } from "better-auth/node";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -11,21 +9,21 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // Replace with your frontend URL if different
+    origin: [`http://localhost:${process.env.FRONTEND_PORT || 3000}`], // Replace with your frontend URL if different
     credentials: true,
   }),
   express.json()
 );
 
-const PORT = process.env.PORT || 5002;
+const AGENT_PORT = process.env.AGENT_PORT || 5002;
 
 const startServer = async () => {
   await connectDB();
 
-  app.listen(PORT, () => {
+  app.listen(AGENT_PORT, () => {
     console.log(
       "Weather agent service connected to DB and is running on PORT:",
-      PORT
+      AGENT_PORT
     );
   });
 };
