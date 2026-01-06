66ccc47f9c5dbc22ee713553c887be88ed24a0d2
Mayank Gupta <mayank.gupta@cube.ms>
2026-01-06 16:28:03 +0530

    feat: Add Navbar component and remove explicit Vite server host and port configuration.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <>
+      <Navbar />
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
   );
 }
 
